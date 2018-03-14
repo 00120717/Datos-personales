2137ab38a44fa730b16ed5421ef907c1685cdd5d
Oscar Lovato <Oscar Lovato@LAPTOP-HN0CBI2R>
2018-03-14 13:18:07 -0600

    TareaDatos

---
+++ src/taream/TareaM.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package taream;
+
+public class TareaM {
+
+    public static void main(String[] args) {
+        String Datos = " Marvin Geovanni Ramirez Vides \n Fecha de nacimiento: 13-02-1999 \n Carnet: 00120717 \n Direccion: Lotificacion El bosque Ciudad Arce \n";
+        int numCel=72539063;
+        System.out.println(Datos + "Telefono: " + numCel);
+        
+    }
+   
+    
+}
